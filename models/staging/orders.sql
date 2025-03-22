bdfd51c93c0f62e77ef2cd49b0da830441b64ab2
mailbox869115@gmail.com <mailbox869115@gmail.com>
2025-03-22 07:25:05 +0000

    commit

---
+++ models/staging/orders.sql
@@ -0,0 +1,10 @@
+{{ config(materialized="table") }}
+
+with
+    tb1 as (
+        select id, order_date, user_id
+
+        from {{ source("datafeed_shared_schema", "RAW_ORDERS") }}
+    )
+select *
+from tb1
