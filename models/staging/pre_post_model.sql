6bba6945d351df3975f50d985b857e7993f55603
shankark k <mailbox869115@gmail.com>
2025-03-24 12:42:12 +0000

    commit

--- models/staging/pre_post_model.sql
+++ models/staging/pre_post_model.sql
@@ -1,6 +1,7 @@
 {{ config(
     materialized="table",
-
+    pre_hook="TRUNCATE TABLE {{ this }}",
+    post_hook="TRUNCATE TABLE {{source('datafeed_shared_schema','RAW_PAYMENTS')}})"
 ) }}
 
 WITH tb1 as(
