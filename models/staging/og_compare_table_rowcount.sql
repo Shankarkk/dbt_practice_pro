4aba64e43631b0f55a9362cf7943df7a50e377ce
shankark k <mailbox869115@gmail.com>
2025-03-26 04:11:15 +0000

    commit

--- models/staging/og_compare_table_rowcount.sql
+++ models/staging/og_compare_table_rowcount.sql
@@ -1,5 +1,6 @@
-{% macro compare_row_count(source_table, target_table) %}
-    WITH source_count AS (
+{{ config(materialized='table') }}
+
+  WITH source_count AS (
         SELECT COUNT(*) AS row_count FROM {{ ref('customers') }}
     ),
     target_count AS (
@@ -14,4 +15,3 @@
             ELSE 'MISMATCH'
         END AS comparison_result
     FROM source_count s join target_count t on 1=1
-{% endmacro %}
